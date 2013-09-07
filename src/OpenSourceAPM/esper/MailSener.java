6de3029f5e027e54406294225e444404626578ba
naerin <jyeong.jeon@gmail.com>
2013-09-08 02:28:09 +0900

    20130908

---
+++ src/OpenSourceAPM/esper/MailSener.java
@@ -0,0 +1,20 @@
+package OpenSourceAPM.esper;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetHeaders;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+
+
+public class MailSener {
+
+}
